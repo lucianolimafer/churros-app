24abd843f7bcdab2cec534d67cec5dc30f41b8fb
Luciano Lima Fer RD <llferreira@rd.com.br>
2024-04-04 00:29:42 -0300

    Initial Commit

---
+++ Churros App/Churros_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Churros_AppApp.swift
+//  Churros App
+//
+//  Created by Luciano Lima Ferreira on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Churros_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
